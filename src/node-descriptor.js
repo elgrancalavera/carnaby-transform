5d0adb12b07d5c3dd7fdc89e5dc777e91726ef95
Leon Coto <leon.coto@mcsaatchi.com>
2014-11-26 12:32:43 +0000

    Adding missing tests

--- src/node-descriptor.js
+++ src/node-descriptor.js
@@ -4,18 +4,27 @@ define(function () {
     'use strict';
     var _ = require('underscore')
 
+    function validateChild(el) {
+        return function(sel) {
+            return sel(el).isValid()
+        }
+    }
+
     return function(rootSel, childSels) {
+        childSels = childSels || []
         return {
-            key: function() {
+            rootKey: function() {
                 return rootSel.selector()
+            },
+            childKeyFor: function(el) {
+                var sel = _.find(childSels, validateChild(el))
+                return !!sel ? sel.selector() : undefined
             },
             isValidRoot: function(el) {
                 return rootSel(el).isValid()
             },
             isValidChild: function(el) {
-                return _.some(childSels || [], function(sel) {
-                    return sel(el).isValid()
-                })
+                return _.some(childSels, validateChild(el))
             }
         }
     }
--- src/specs/node-descriptor.js
+++ src/specs/node-descriptor.js
@@ -11,11 +11,20 @@ define(function (require) {
             it('Should be a valid root', function() {
                 expect(f.viewDescriptor.isValidRoot(f.singleView)).to.be.ok()
             })
-            it('Should be an invalid root', function() {
+            it('Unknown node be an invalid root', function() {
                 expect(f.viewDescriptor.isValidRoot(f.unknown)).not.to.be.ok()
+            })
+            it('Known node but wrong description', function() {
+                expect(f.viewDescriptor.isValidRoot(f.singleApp)).not.to.be.ok()
             })
             it('Should be a valid child', function() {
                 expect(f.viewDescriptor.isValidChild(f.singleView)).to.be.ok()
+            })
+            it('Known node but invalid child', function() {
+                expect(f.viewDescriptor.isValidChild(f.singleApp)).not.to.be.ok()
+            })
+            it('Unknown node', function() {
+                expect(f.viewDescriptor.isValidChild(f.unknown)).not.to.be.ok()
             })
         })
 
@@ -24,6 +33,21 @@ define(function (require) {
                 expect(f.componentDescriptor.isValidChild(f.singleComponent)).to.be.ok()
                 expect(f.componentDescriptor.isValidChild(f.singleController)).to.be.ok()
                 expect(f.componentDescriptor.isValidChild(f.singleView)).to.be.ok()
+            })
+        })
+
+        describe('retrieving keys', function() {
+            it('A root descriptor key should match a selector', function() {
+                expect(f.viewDescriptor.rootKey()).to.equal(f.viewSelector.selector())
+            })
+            it('A child descriptor key should match a selector', function() {
+                expect(f.componentDescriptor.childKeyFor(f.singleController)).to.equal(f.controllerSelector.selector())
+            })
+            it('A known selector not allowed by the descriptor should not return a key', function() {
+                expect(f.viewDescriptor.childKeyFor(f.singleApp)).not.to.be.ok()
+            })
+            it('An unknown child should not return a key', function() {
+                expect(f.viewDescriptor.childKeyFor(f.unknown)).not.to.be.ok()
             })
         })
     })
