e652395e9b95fcc57e30b987ce9101dcfca5e0e8
Leon Coto <leon.coto@mcsaatchi.com>
2014-12-15 12:49:24 +0000

    Making sure to update the version in the gruntfile config as well

--- tasks/release.js
+++ tasks/release.js
@@ -4,7 +4,7 @@ var semver = require('semver')
 module.exports = function(grunt) {
 
     function writeJSON(filepath, object) {
-        grunt.file.write(filepath, JSON.stringify(object, null, 4))
+        grunt.file.write(filepath, JSON.stringify(object, null, 2))
     }
 
     function nextVersion(level) {
@@ -21,6 +21,8 @@ module.exports = function(grunt) {
         grunt.config('files.meta').forEach(function(filepath) {
             bumpVersion(filepath, version)
         })
+        grunt.config('pkg', grunt.file.readJSON('package.json'))
+        grunt.log.ok('Version bumped to: ' + grunt.config('pkg.version'))
     }
 
     function release(version) {
