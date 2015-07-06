d5fd2200f0437033e27b6549918aa016ae933656
Leon Coto <leoncoto@gmail.com>
2015-07-06 23:47:26 +0100

    Cleanup

--- wrap/start.js
+++ wrap/start.js
@@ -1,11 +1,3 @@
-//--------------------------------------------------------------------------
-//
-// carnaby.transform
-// Copyright (c) <%= grunt.template.today('yyyy') %> M&C Saatchi
-// Distributed under MIT license
-//
-//--------------------------------------------------------------------------
-
 ;(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['underscore'], factory);
