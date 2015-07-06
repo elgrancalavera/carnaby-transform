96668b0c656cc0a78461cbcbc56e632337e9eaf8
Leon Coto <leoncoto@gmail.com>
2015-07-06 22:48:40 +0100

    Cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -2,289 +2,203 @@
 
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
+    jshint: {
+      grunt: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
         files: {
-            grunt: [
-                'gruntfile.js',
-                'tasks/**/*.js'
-            ],
-            specs: [
-                'test/specs/**/*.js'
-            ],
-            specsrunner: [
-                'index.html'
-            ],
-            src: [
-                'src/**/*.js',
-                '!<%= files.specs %>'
-            ],
-            meta: [
-                'package.json',
-                'bower.json'
+          src: [ 'gruntfile.js', 'tasks/**/*.js' ]
+        }
+      },
+      test: {
+        options: {
+          jshintrc: 'test/specs/.jshintrc'
+        },
+        files: {
+          src: [ 'test/specs/**/*.js' ]
+        }
+      },
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        files: {
+          src: [ 'src/**/*.js' ]
+        }
+      },
+    },
+
+    watch: {
+      grunt: {
+        files: '<%= jshint.grunt.files.src %>',
+        tasks: [
+          'jshint:grunt',
+        ]
+      },
+      test: {
+        options: {
+          livereload: true
+        },
+        files: [
+          '<%= jshint.test.files.src %>',
+          'test/index.html',
+        ],
+        tasks: [
+          'jshint:test',
+          'mocha:dev',
+        ]
+      },
+      src: {
+        options: {
+          livereload: true
+        },
+        files: '<%= jshint.src.files.src %>',
+        tasks: [
+          'jshint:src',
+          'mocha:dev',
+        ]
+      },
+    },
+
+    connect: {
+      test: {
+        options: {
+          hostname: 'localhost',
+          port: grunt.option('connectPort') || 9000,
+          base: '.'
+        }
+      }
+    },
+
+    mocha: {
+      options: {
+        run: false,
+        timeout: grunt.option('timeout') || 5000,
+        reporter: 'Spec'
+      },
+      dev: {
+        options: {
+          urls: [
+            'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test'
+          ]
+        }
+      },
+      dist: {
+        options: {
+          urls: [
+            'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test/?dist=true',
+            'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test/?dist=true&min=true'
+          ]
+        }
+      }
+    },
+
+    requirejs: {
+      dist: {
+        options: {
+          baseUrl: 'src',
+          dir: '.tmp',
+          optimize: 'none',
+          paths: {
+            underscore: 'empty:',
+            jquery: 'empty:'
+          },
+          modules: [{
+            name: 'transform',
+            include: [
+              'selector',
+              'rule'
             ]
+          }]
+        }
+      }
+    },
+
+    uglify: {
+      dist: {
+        options: {
+          sourceMap: true,
         },
+        files: {
+          'dist/transform.min.js': [ 'dist/transform.js' ]
+        }
+      }
+    },
 
-        paths: {
-            requirejs: {
-                base: 'src',
-                config: 'src/main.js',
-                build: 'build'
-            },
-            wrap: {
-                start: 'wrap/start.js',
-                end: 'wrap/end.js',
-            },
-            dist: 'dist'
-        },
+    releaser: {
+      options: {
+        additionalFiles: [ 'bower.json' ],
+        tagName: 'v<%= version %>',
+      },
+      bump: {
+        options: {
+          bump: true,
+          add: true,
+          commit: true,
+          push: false,
+          tag: false,
+          pushTags: false,
+          npm: false,
+          reloadpkg: true
+        }
+      },
+      release: {
+        options: {
+          bump: false,
+          commit: false,
+          add: false,
+          push: true,
+          tag: true,
+          pushTags: true,
+          npm: true,
+        }
+      }
+    },
 
-        //----------------------------------
-        //
-        // jshint
-        //
-        //----------------------------------
+    bundle: {
+      options: {
+        src: '.tmp/transform.js',
+        dest: 'dist/transform.js',
+        wrapper: {
+          start: 'wrap/start.js',
+          end: 'wrap/end.js'
+        }
+      }
+    },
 
-        jshint: {
-            grunt: {
-                options: {
-                    jshintrc: '.jshintrc'
-                },
-                files: {
-                    src: '<%= files.grunt %>'
-                }
-            },
-            specs: {
-                options: {
-                    jshintrc: 'test/specs/.jshintrc'
-                },
-                files: {
-                    src: '<%= files.specs %>'
-                }
-            },
-            src: {
-                options: {
-                    jshintrc: 'src/.jshintrc'
-                },
-                files: {
-                    src: '<%= files.src %>'
-                }
-            },
-        },
+    clean: [ 'dist', '.tmp' ]
+  })
 
-        //----------------------------------
-        //
-        // watch
-        //
-        //----------------------------------
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks)
+  grunt.loadTasks('tasks')
 
-        watch: {
-            grunt: {
-                files: '<%= files.grunt %>',
-                tasks: [
-                    'jshint:grunt',
-                ]
-            },
-            test: {
-                options: {
-                    livereload: true
-                },
-                files: [
-                    '<%= files.specs %>',
-                    '<%= files.specsrunner %>',
-                ],
-                tasks: [
-                    'jshint:specs',
-                    'mocha:dev',
-                ]
-            },
-            src: {
-                options: {
-                    livereload: true
-                },
-                files: '<%= files.src %>',
-                tasks: [
-                    'jshint:src',
-                    'mocha:dev',
-                ]
-            },
-        },
+  grunt.registerTask(
+    'default',
+    'Build',
+    [
+      'jshint',
+      'clean',
+      'requirejs:dist',
+      'bundle',
+      'uglify:dist',
+      'connect:test',
+      'mocha:dev',
+      'mocha:dist'
+    ]
+  )
 
-        //----------------------------------
-        //
-        // connect
-        //
-        //----------------------------------
-
-        connect: {
-            test: {
-                options: {
-                    hostname: 'localhost',
-                    port: grunt.option('connectPort') || 9000,
-                    base: '.'
-                }
-            }
-        },
-
-        //----------------------------------
-        //
-        // mocha
-        //
-        //----------------------------------
-
-        mocha: {
-            options: {
-                // requirejs will call `mocha.run()`
-                run: false,
-                timeout: grunt.option('timeout') || 5000,
-                reporter: 'Spec'
-            },
-            dev: {
-                options: {
-                    urls: [
-                        'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test'
-                    ]
-                }
-            },
-            dist: {
-                options: {
-                    urls: [
-                        'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test/?dist=true',
-                        'http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/test/?dist=true&min=true'
-                    ]
-                }
-            }
-        },
-
-        //----------------------------------
-        //
-        // requirejs
-        //
-        //----------------------------------
-
-        requirejs: {
-            dist: {
-                options: {
-                    baseUrl: 'src',
-                    dir: '.tmp',
-                    optimize: 'none',
-                    paths: {
-                        underscore: 'empty:',
-                        jquery: 'empty:'
-                    },
-                    modules: [{
-                        name: 'transform',
-                        include: [
-                            'selector',
-                            'rule'
-                        ]
-                    }]
-                }
-            }
-        },
-
-        //----------------------------------
-        //
-        // uglify
-        //
-        //----------------------------------
-
-        uglify: {
-            dist: {
-                options: {
-                    sourceMap: true,
-                },
-                files: {
-                    'dist/transform.min.js': ['dist/transform.js']
-                }
-            }
-        },
-
-        //----------------------------------
-        //
-        // releaser
-        //
-        //----------------------------------
-
-        releaser: {
-            options: {
-                additionalFiles: [ 'bower.json' ],
-                tagName: 'v<%= version %>',
-            },
-            bump: {
-                options: {
-                    bump: true,
-                    add: true,
-                    commit: true,
-                    push: false,
-                    tag: false,
-                    pushTags: false,
-                    npm: false,
-                    reloadpkg: true
-                }
-            },
-            release: {
-                options: {
-                    bump: false,
-                    commit: false,
-                    add: false,
-                    push: true,
-                    tag: true,
-                    pushTags: true,
-                    npm: true,
-                }
-            }
-        },
-
-        bundle: {
-            options: {
-                src: '.tmp/transform.js',
-                dest: 'dist/transform.js',
-                wrapper: {
-                    start: '<%= paths.wrap.start %>',
-                    end: '<%= paths.wrap.end %>'
-                }
-            }
-        },
-
-        //----------------------------------
-        //
-        // clean
-        //
-        //----------------------------------
-
-        clean: [
-            'dist',
-            '.tmp'
-        ]
-    })
-
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks)
-    grunt.loadTasks('tasks')
-
-    grunt.registerTask(
-        'default',
-        'Build',
-        [
-            'jshint',
-            'clean',
-            'requirejs:dist',
-            'bundle',
-            'uglify:dist',
-            'connect:test',
-            'mocha:dev',
-            'mocha:dist'
-        ]
-    )
-
-    grunt.registerTask(
-        'dev',
-        'Lints, starts connect and watches for changes.',
-        [
-            'jshint',
-            'connect:test',
-            'watch',
-        ]
-    )
+  grunt.registerTask(
+    'dev',
+    'Lints, starts connect and watches for changes.',
+    [
+      'jshint',
+      'connect:test',
+      'watch',
+    ]
+  )
 }
